6a535f62cf86ed00c8e897ecc9c0b79f0ddfc1be
Dmytro Zykov <dmytri.zykov@gmail.com>
2023-06-08 00:09:14 +0300

    added command line options

--- RayTracer/src/main.cpp
+++ RayTracer/src/main.cpp
@@ -1,11 +1,100 @@
+#include <filesystem>
 #include <iostream>
+
+#include <boost/algorithm/string.hpp>
+#include <boost/program_options.hpp>
+#include <fmtlog/fmtlog.h>
+
+#include <icl/image.h>
 
 #include "objects/disk.h"
 #include "objects/sphere.h"
 #include "render/renderer.h"
 
-int main()
+namespace
 {
+constexpr auto kSource{ "source" };
+constexpr auto kOutput{ "output" };
+constexpr auto kObjFileExtension{ "obj" };
+} // namespace
+
+std::shared_ptr< boost::program_options::options_description >
+program_options( const int argc,
+                 const char**& argv,
+                 std::string& source_file,
+                 std::string& output_file,
+                 bool& is_help )
+{
+    // clang-format off
+    auto desc { std::make_shared<boost::program_options::options_description>( "RayTracer usage") };
+    desc->add_options()
+      ( kSource,
+        boost::program_options::value< std::string >( &source_file ),
+        "(required) input Wavefront OBJ file" )
+      ( kOutput,
+        boost::program_options::value< std::string >(&output_file),
+        "(required) location to output file" )
+      ( "help, h",
+        boost::program_options::bool_switch( &is_help ),
+        "display a help dialog" );
+    // clang-format on
+    boost::program_options::variables_map map_of_args;
+    boost::program_options::store( boost::program_options::parse_command_line( argc, argv, *desc ),
+                                   map_of_args );
+    boost::program_options::notify( map_of_args );
+
+    return desc;
+}
+
+bool checkIfFileExists( const std::string& file_name )
+{
+    const std::filesystem::path path_to_file_name{ file_name.c_str() };
+    return std::filesystem::exists( path_to_file_name );
+}
+
+std::string getExtension( const std::string& file_path )
+{
+    return file_path.substr( file_path.rfind( '.' ) + 1,
+                             file_path.size() - file_path.rfind( '.' ) - 1 );
+}
+
+int main( int argc, char** argv )
+{
+    std::string source_file, output_file;
+    bool is_help{};
+
+    const auto desc{ program_options( argc,
+                                      const_cast< const char**& >( argv ),
+                                      source_file,
+                                      output_file,
+                                      is_help ) };
+
+    if ( is_help || source_file.empty() || output_file.empty() )
+    {
+        std::cout << *desc << std::endl;
+        return EXIT_SUCCESS;
+    }
+
+    const auto source_file_extension{ getExtension( source_file ) };
+    if ( !boost::iequals( source_file_extension, kObjFileExtension ) )
+    {
+        loge( "Unsupported input format! Only Wavefront OBJ are supported." );
+        return EXIT_FAILURE;
+    }
+
+    if ( !checkIfFileExists( source_file ) )
+    {
+        loge( "Input file not found: {}", source_file );
+        return EXIT_FAILURE;
+    }
+
+    icl::ImageFormat image_output_format{ getExtension( output_file ) };
+    if ( image_output_format == icl::ImageFormat::kUndefined )
+    {
+        loge( "Unsupported image output format! RayTracer support only PPM and BMP images." );
+        return EXIT_FAILURE;
+    }
+
     render::Renderer renderer{ 90.0f, 150, 150 };
     auto& rendererScene{ renderer.getScene() };
 
@@ -20,4 +109,6 @@ int main()
                                        lmath::Point3{ 0.0f, 0.0f, -2.0f } ) );
 
     renderer.render();
+
+    return EXIT_SUCCESS;
 }
